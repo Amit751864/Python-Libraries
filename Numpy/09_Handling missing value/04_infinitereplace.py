9e5c28bb60a37d07579df131a84112a6187f7909
Amit_03 <kumarguptaa254@gmail.com>
2026-07-31 01:31:26 +0530

    mul

--- Numpy/09_Handling missing value/04_infinitereplace.py
+++ Numpy/09_Handling missing value/04_infinitereplace.py
@@ -1,6 +1,7 @@
-#np.isinf(array)
 import numpy as np
 arr = np.array([1,2,3,np.inf,4,8,-np.inf,7])
+
 print(np.isinf(arr))
 
-# give true  should be present np.inf(infinity value) persent
+cleaned_arr = np.nan_to_num(arr,posinf=1000,neginf=-1000)
+print(cleaned_arr)

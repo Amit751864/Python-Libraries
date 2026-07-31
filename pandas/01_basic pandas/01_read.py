a0301416d4a62c5e44ec36f7aacf60f0b2edeb5a
Amit_03 <kumarguptaa254@gmail.com>
2026-07-31 15:59:32 +0530

    spa

--- pandas/01_basic pandas/01_read.py
+++ pandas/01_basic pandas/01_read.py
@@ -1,6 +1,4 @@
 import pandas as pd
-
-
 # read  data from CSV file into a dataframe
 #df = pd.read_csv("simpled_data.csv",encoding="latin1")
 #df = pd.read_excel("SampleSuperstore.xlsx",encoding="latin1")

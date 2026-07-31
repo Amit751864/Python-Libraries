286983942280aa4421cf7c76406df0682f43537e
Amit_03 <kumarguptaa254@gmail.com>
2026-07-31 15:36:52 +0530

    que

--- Numpy/Practice Questions/p17.py
+++ Numpy/Practice Questions/p17.py
@@ -3,5 +3,5 @@ import numpy as np
 arr = np.array([[[10,23,23],
                  [89,34,24],
                  [88,45,23]]])
-row_wise_sum = np.sum(arr,axis 0)
+row_wise_sum = np.sum(arr,axis= 0)
 print(row_wise_sum)
---
+++ Numpy/Practice Questions/p18.py
@@ -0,0 +1,5 @@
+# multiply every element by 8
+import numpy as np
+arr = np.array([[1,2,3],
+                [9,4,3]])
+print(arr*8)
